b0ccddd25e6b3bd0080c158a857c11ddae5ee490
ehosaini <me.hosaini@gmail.com>
2018-08-05 21:19:52 -0400

    Add templates and placeholders for user ride history views

--- routes/userRides.js
+++ routes/userRides.js
@@ -4,9 +4,19 @@ const router = express.Router();
 const userModelHelpers = require('./../models/model-helpers/userModelHelper');
 
 
-/* GET users listing. */
+/* Show users history in tabular form. */
 router.get('/', function(req, res, next) {
-  res.send('User homepage');
+  res.render('myridesTable', {
+    title: 'User Rides | Table'
+  });
 });
+
+/* Plot user history on Google Maps */
+router.get('/map', function(req, res, next) {
+  res.render('myridesMap', {
+    title: 'User Rides | Map'
+  });
+});
+
 
 module.exports = router;
