ca386b6e49e8b3a4684dc5b5441e20316af280fc
ehosaini <me.hosaini@gmail.com>
2018-08-31 17:51:07 -0400

    Inform users if they don't have history

--- public/javascripts/myridesTable.js
+++ public/javascripts/myridesTable.js
@@ -12,15 +12,22 @@ $(document).ready(async () => {
 
         // Order cities by trip counts in descending order
         const orderedHistory = _.orderBy(userHistory, ['trips_count'], ['desc']);
+        console.log(orderedHistory.length)
 
-        // Hide the loading cue prior to populating the table
-        dataLoadCue.css('display', 'none');
+        if (orderedHistory.length !== 0) {
+            // Hide the loading cue prior to populating the table
+            dataLoadCue.css('display', 'none');
+            // Populate user's history table
+            orderedHistory.forEach((history) => {
+                const row = `<tr><th>${history['city']['display_name']}</th><td>${history['trips_count']}</td></tr>`
+                $('#trips-summary').append(row);
+            });
+        };
 
-        // Populate user's history table
-        orderedHistory.forEach((history) => {
-            const row = `<tr><th>${history['city']['display_name']}</th><td>${history['trips_count']}</td></tr>`
-            $('#trips-summary').append(row);
-        });
+        const text = '<h4 class="text-danger">You do not have a ride history with Uber.</h4>'
+        console.log(text);
+        dataLoadCue.html(text);
+
     } else {
         // Hide the loading cue and show message
         dataLoadCue.css('display', 'none');
